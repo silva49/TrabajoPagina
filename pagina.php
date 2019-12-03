27f5527215f4ea353f0875ec7d3d7b65c5ffa0ed
cristian tilano silva <cristiantilanosilva@gmail.com>
2019-12-03 18:51:01 -0500

    trabajo pagina web

---
+++ pagina.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        <title>
+           Atletico Nacional
+        </title>
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+        <div id="pagewrap">
+                <img id="logo1" src="images/logo.jpg">
+                <img id="logo2" src="images/logo.jpg">
+    <center>
+        <div id="header">
+
+                <h1>Atletico nacional</h1>
+                <p>Inicio - Mision - Vision - Contactenos</p>
+                
+
+        </div>
+    </center>
+<div id="content">
+            <h2>Historia</h2>
+            <p>El Atlético Nacional de Medellin es un club de fútbol de Medellín,
+              Antioquia, Colombia. Es considerado uno de los clubes más grandes y populares de Colombia y uno de los más importantes en Sudamérica.11​ Fundado el 7 de marzo de 1947 bajo el nombre de Club Atlético Municipal de Medellín, aunque (por escritura pública esta sociedad) fue constituida el 30 de abril de 1947, Luis Alberto Villegas, cambió el nombre del club por Club Atlético Nacional.</p>
+        
+            <h2>Nuestra hinchada</h2>
+            <p>El Atlético Nacional de Medellin es un club de fútbol de Medellín, Antioquia, Colombia. Es considerado uno de los clubes más grandes y populares de Colombia y uno de los más importantes en Sudamérica.11​ Fundado el 7 de marzo de 1947 bajo el nombre de Club Atlético Municipal de Medellín, aunque (por escritura pública esta sociedad) fue constituida el 30 de abril de 1947, Luis Alberto Villegas, cambió el nombre del club por Club Atlético Nacional</p>
+
+        <div id="valor">
+            <h2>Rivalidad</h2>
+            Es el partido que enfrenta a los dos principales equipos del Valle de Aburrá y propiamente los únicos clubes profesionales de la
+             ciudad de Medellín: Atlético Nacional e Independiente Medellín. Es uno de los clásicos tradicionales del fútbol colombiano, El primer clásico que se disputó en la historia entre ambos equipos fue el 12 de septiembre de 1948 y la victoria se la llevó el Independiente Medellín superando al que en el momento se denominaba Atlético Municipal
+        </div>            
+        
+        <div id="lista">
+            <h2>Lista de titulos </h2>
+            <p>Integridad: Ser auténticos.
+                <p>
+                liga colombiana: 16 titulos
+                <p>    
+                Copa libertadores : 2 copas
+                <p>    
+                interamericana : 2 titulos
+                <p>    
+                copa colombia: 4 titulos 
+                <p>    
+                Diversidad: Tan incluyente como nuestras marcas.
+                <p>   
+                superliga: 2 titulos
+            </p>
+        </div>
+</div>
+
+        <div id="sidebar1">
+
+            <h3>Formacion</h3>
+            <p>intención de reforzar al equipo y tener una buena temporada, se acordó fusionarlo con otro club aficionado de la región llamado Indulana Foot-Ball Club, de esta forma el nuevo club pasó a llamarse Unión Indulana Foot-Ball Club.  </p>
+        </div>
+
+        <div id="sidebar2">
+
+            <h3>primer titulo:</h3>
+            <p>
+            El Atlético Municipal no tuvo mayor figuración durante los primeros torneos del profesionalismo. Era la época de El Dorado en la que llegaron a Colombia jugadores de toda América, entre ellos, varios que en aquel momento estaban entre los mejores del mundo, pero el club mantenía su política de tener únicamente jugadores nacidos en Colombia, conocidos como los «puros criollos», esto lo situó en franca desventaja frente al resto de equipos que se habían reforzado con extranjeros. En agosto de 1950 el nombre del equipo se cambió por el de Club Atlético Nacional. 
+        </p>
+        
+        </div>
+<center>
+        <div id="footer">
+                <img id="botella1" src="images/tigo.jpg">
+                <img id="botella2" src="images/tigo.jpg">
+        <p>
+            Contactenos:
+            <p>
+                TIENDA VERDE 2222210
+            <p>
+            atleticonacional@hotmail.com
+            <p>
+                cristian tilano slva
+            <p>
+                Analisis y Desarrollo de Software - censa
+        </p>
+        
+        </div>
+</center>
+
+</div>
+
+        
+    </body>
+</html>
